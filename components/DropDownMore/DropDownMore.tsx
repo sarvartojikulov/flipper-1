2155fe165c02eb48c265416e9385f73a98e55117
derekcovey <coveydrk@gmail.com>
2021-06-18 16:35:19 +0200

    feat: add DropDwonMore component which appears on click to three-dot button on the DeckCard component

---
+++ components/DropDownMore/DropDownMore.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FC } from 'react';
+import styles from './DropDownMore.module.scss';
+
+const DropDownMore: FC = () => {
+
+
+	const  DropDownMoreItem: FC = () => {
+
+
+		return (
+			<>
+			
+			</>
+		)
+	}
+
+	return (
+		<div className={styles.body}>
+			
+		</div>
+	);
+};
+
+export default DropDownMore;
