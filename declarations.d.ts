02860c6eb085b8c3fec224f0ab3c954e582c196a
derekcovey <coveydrk@gmail.com>
2021-06-29 11:05:55 +0200

    conf: add react-transition-group dependency, update config files, add declaration for SVG components

--- declarations.d.ts
+++ declarations.d.ts
@@ -2,3 +2,5 @@ declare module "*.scss" {
     const content: { [className: string]: string };
     export = content;
 }
+
+declare module "\*.svg" {  import React = require("react");  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;  const src: string;  export default src;}
