ded5e1fce6fd7419e6a2c20bbb78d28b4321c859
derekcovey <coveydrk@gmail.com>
2021-06-29 15:19:33 +0200

    feat: add dropdown(for now fadeout :)) menu component

--- components/DropDownMore/DropDownMore.tsx
+++ components/DropDownMore/DropDownMore.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import { FC } from 'react';
-import styles from './DropDownMore.module.scss';
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { FC } from "react";
+import styles from "./DropDownMore.module.scss";
+import DropDownMoreItem from "../DropDownMoreItem/DropDownMoreItem";
+import PencilSVG from "../../icons/PencilSVG";
+import ManageAccessSvg from "../../icons/ManageAccessSVG";
+import PinSVG from "../../icons/PinSVG";
+import DeleteSVG from "../../icons/DeleteSVG";
 
-const DropDownMore: FC = () => {
+interface DropDownMoreProps {
+  closePanel?: () => void;
+  dataShowMore?: boolean;
+}
+
+const DropDownMore: FC<DropDownMoreProps> = ({ closePanel, dataShowMore }) => {
+  const [close, setClose] = useState<boolean>(false);
 
 
-	const  DropDownMoreItem: FC = () => {
+  return (
+    <div>
+      <div
+        className={styles.wrapper}
+        onClick={() => {
+          closePanel();
+        }}
+      ></div>
 
-
-		return (
-			<>
-			
-			</>
-		)
-	}
-
-	return (
-		<div className={styles.body}>
-			
-		</div>
-	);
+      <div className={styles.body} data-showmore={dataShowMore || close}>
+        <DropDownMoreItem text="Edit" onClickHandle={() => {}}>
+          <PencilSVG />
+        </DropDownMoreItem>
+        <DropDownMoreItem text="Manage Access" onClickHandle={() => {}}>
+          <ManageAccessSvg />
+        </DropDownMoreItem>
+        <DropDownMoreItem text="Pin" onClickHandle={() => {}}>
+          <PinSVG />
+        </DropDownMoreItem>
+        <DropDownMoreItem text="Delete" onClickHandle={() => {}}>
+          <DeleteSVG />
+        </DropDownMoreItem>
+      </div>
+    </div>
+  );
 };
 
 export default DropDownMore;
