e2bf5be2930bb500ce35e9da945d0dcc6abf13f8
derekcovey <coveydrk@gmail.com>
2021-06-29 15:22:17 +0200

    refactor: move svg(burger icon) to SVG icon react component

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,32 +1,18 @@
-import React, {FC} from 'react';
-import styles from './NavBar.module.scss'
+import React, { FC } from "react";
+import styles from "./NavBar.module.scss";
+import BurgerSVG from "../../icons/BurgerSVG";
+import { useState } from "react";
 
-const NavBar: FC = props => {
-    return (
-        <div className={styles.wrapper}>
-            <span className="text-xl font-semibold">
-            Your Decks
+const NavBar: FC = () => {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
 
-            </span>
+  return (
+    <div className={styles.wrapper}>
+      <span className="text-xl font-semibold">Your Decks</span>
 
-            <svg width="29" height="17" viewBox="0 0 29 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M0 1H29M0 8.5H29M0 16H29" stroke="#1D2228" strokeWidth="2"/>
-            </svg>
-
-        </div>
-            // <div className="h-14 px-6 flex justify-between items-center">
-            //     <span className="text-xl font-semibold">
-            //     Your Decks
-
-            //     </span>
-
-            //     <svg width="29" height="17" viewBox="0 0 29 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-            //         <path d="M0 1H29M0 8.5H29M0 16H29" stroke="#1D2228" stroke-width="2"/>
-            //     </svg>
-
-            // </div>
-    );
+      <BurgerSVG onClick={() => setShowMenu(!showMenu)} />
+    </div>
+  );
 };
-
 
 export default NavBar;
