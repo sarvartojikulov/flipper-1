e4c22af548f7b172df24458117f56ea6193eae70
Derek <diyorbektojikulov@senstill.pl>
2021-07-01 13:19:03 +0200

    feat: add transitions for sidemenu, refactor some code.

--- components/SideMenu/SideMenu.tsx
+++ components/SideMenu/SideMenu.tsx
@@ -2,6 +2,7 @@ import React, {FC} from "react";
 import styles from "./SideMenu.module.scss"
 import { useState } from "react";
 import BurgerSVG from "../../icons/BurgerSVG";
+<<<<<<< HEAD
 
 const SideMenu: FC = () => {
 
@@ -24,3 +25,81 @@ const SideMenu: FC = () => {
 
 
 export default SideMenu;
+=======
+import SideMenuItem from "../SideMenuItem/SideMenuItem";
+import ProfileSVG from "../../icons/ProfileSVG";
+import HomeSVG from "../../icons/HomeSVG";
+import SettingsSVG from "../../icons/SettingsSVG";
+import { CSSTransition } from "react-transition-group"
+
+const SideMenu: FC = () => {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+
+
+   let animations = {
+    enter: styles.sideMenuEnter,
+    enterActive: styles.sideMenuEnterActive,
+    exit: styles.sideMenuExit,
+    exitActive: styles.sideMenuExitActive
+}
+
+ let animationsMask = {
+    enter: styles.maskEnter,
+    enterActive: styles.maskEnterActive,
+    exit: styles.maskExit,
+    exitActive: styles.maskExitActive
+}
+
+  return (
+    <div>
+      <BurgerSVG onClick={() => setShowMenu(!showMenu)} />
+
+
+      <CSSTransition in={showMenu} timeout={300} unmountOnExit classNames={{...animationsMask}}>
+        <div
+          className={styles.hideMenu}
+          onClick={() => setShowMenu(false)}
+        ></div>
+
+      </CSSTransition>
+
+      <CSSTransition in={showMenu} timeout={300} unmountOnExit classNames={{...animations}}>
+        <div className={styles.menuBody}>
+          <div className={styles.menuHeader}>
+            <div className={styles.avatar}></div>
+            <span>Hey, sw_derek</span>
+          </div>
+          <svg
+            width="268"
+            height="2"
+            viewBox="0 0 268 2"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1 1L267 1"
+              stroke="#A3A3A3"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+
+          <SideMenuItem text="Profile">
+            <ProfileSVG />
+          </SideMenuItem>
+          <SideMenuItem text="Home">
+            <HomeSVG />
+          </SideMenuItem>
+          <SideMenuItem text="Settings">
+            <SettingsSVG />
+          </SideMenuItem>
+        </div>
+
+
+      </CSSTransition>
+    </div>
+  );
+};
+
+export default SideMenu;
+>>>>>>> ef0f20d... feat: add transitions for sidemenu, refactor some code.
